bed3b75c75a5e392bdfae49d0033dd0c96de425c
Andresjm30 <53202888+Andresjm30@users.noreply.github.com>
2022-01-28 21:15:38 -0500

    Creating Pie Chart using JavaFX

---
+++ Project #3/DrawDesign.java
@@ -0,0 +1,40 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+    
+   
+   public class DrawDesign extends Application {
+   
+      @Override
+      public void start(Stage stage) throws Exception{
+   
+          stage.setTitle("Pie Chart");
+          Canvas canvas = new Canvas(800, 800);
+          GraphicsContext gc = canvas.getGraphicsContext2D();
+   
+          String s = "C:\\Users\\good_\\Desktop\\emma.txt";
+          HistgramLetters emma = new HistgramLetters(s);
+          emma.setLetters_probabilities(emma.letters_frequencies);
+          emma.setLetters_angles(emma.letters_probabilities);
+          emma.setNSorted(emma.letters_frequencies);
+   
+          PieChart chart = new PieChart(emma.getLetter_probabilites(), emma.getLetters_angles(), emma.getNSorted());
+   
+          Pane root = new Pane();
+          root.getChildren().add(canvas);
+          chart.nInput(gc, root, gc.getCanvas().getWidth(), gc.getCanvas().getWidth());//Input and button calls drawChart
+   
+          Scene scene = new Scene(root);
+          stage.setScene(scene);
+          stage.show();
+      }
+   
+   
+      public static void main(String[] args) {
+          launch(args);
+      }
+   
+   }
